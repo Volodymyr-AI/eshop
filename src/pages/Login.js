0d79fb0200b257ac29f4d828701162b993ed61c2
Volodymyr <alternativeqoob@gmail.com>
2022-04-07 15:38:52 +0300

    version 0.1.0

---
+++ src/pages/Login.js
@@ -0,0 +1,80 @@
+import React, {useContext, useState} from 'react';
+import {Container, Form} from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import {NavLink, useLocation, useNavigate} from "react-router-dom";
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
+
+const Login = observer(() => {
+    const {user} = useContext(Context)
+    const location = useLocation()
+    const history = useNavigate()
+    const isLogin = location.pathname === LOGIN_ROUTE
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const click = async () => {
+        try {
+            let data;
+            if (isLogin) {
+                data = await login(email, password);
+            } else {
+                data = await registration(email, password);
+            }
+            user.setUser(user)
+            user.setIsLogin(true)
+            history.push(SHOP_ROUTE)
+        } catch (e) {
+            alert(e.response.data.message)
+        }
+
+    }
+
+    return (
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">{isLogin ? 'Авторизація' : "Регістрація"}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введіть e-mail..."
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введіть ваш пороль..."
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        type="password"
+                    />
+                    <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                        {isLogin ?
+                            <div>
+                                Нема аккаунту? <NavLink to={REGISTRATION_ROUTE}>Зареєструйся!</NavLink>
+                            </div>
+                            :
+                            <div>
+                               Є аккаунт? <NavLink to={LOGIN_ROUTE}>Увійди!</NavLink>
+                            </div>
+                        }
+                        <Button
+                            variant={"outline-success"}
+                            onClick={click}
+                        >
+                            {isLogin ? 'Увійти' : 'Реєстрація'}
+                        </Button>
+                    </Row>
+
+                </Form>
+            </Card>
+        </Container>
+    );
+});
+
+export default Login;
